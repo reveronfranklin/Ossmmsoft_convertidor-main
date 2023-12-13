bb299ad170080bab452eb4f99893bce95da04540
reveronfranklin <reveronfranklin@gmail.com>
2023-12-13 17:37:35 -0400

    Correccion Funtion getdatbyobjec,resta mes 1, mane el indice del mes

--- Convertidor/Dtos/Presupuesto/GetPRE_PRESUPUESTOSDto.cs
+++ Convertidor/Dtos/Presupuesto/GetPRE_PRESUPUESTOSDto.cs
@@ -8,11 +8,22 @@ namespace Convertidor.Dtos.Presupuesto
         public string Descripcion { get; set; } = string.Empty;
         public int Ano { get; set; }
         public decimal MontoPresupuesto { get; set; }
-        public string FechaDesde { get; set; } = string.Empty;
-        public string FechaHasta { get; set; } = string.Empty;
-        public string FechaAprobacion { get; set; } = string.Empty;
+        
+        public DateTime FechaDesde { get; set; } 
+        public DateTime FechaHasta { get; set; } 
+        public string FechaDesdeString { get; set; } = string.Empty;
+        public string FechaHastaString { get; set; } = string.Empty;
+        public FechaDto FechaDesdeObj { get; set; }
+        public FechaDto FechaHastaObj { get; set; }
+        
+        public DateTime FechaAprobacion { get; set; }
+        public string FechaAprobacionString { get; set; } = string.Empty; 
+        public FechaDto FechaAprobacionObj { get; set; }
+        
         public string NumeroOrdenanza { get; set; } = string.Empty;
-        public string FechaOrdenanza { get; set; } = string.Empty;
+        public DateTime FechaOrdenanza { get; set; }
+        public string FechaOrdenanzaString { get; set; } = string.Empty;
+        public FechaDto FechaOrdenanzaObj { get; set; }
         public string Extra1 { get; set; } = string.Empty;
         public string Extra2 { get; set; } = string.Empty;
         public string Extra3 { get; set; } = string.Empty;
@@ -29,10 +40,9 @@ namespace Convertidor.Dtos.Presupuesto
         public string TotalVigenteString { get; set; } = string.Empty;
 
 
-        public FechaDto FechaDesdeObj { get; set; }
-        public FechaDto FechaHastaObj { get; set; }
-        public FechaDto FechaAprobacionObj { get; set; }
-        public FechaDto FechaOrdenanzaObj { get; set; }
+      
+   
+    
         public List<PreFinanciadoDto>? PreFinanciadoDto { get; set; }
         public List<GetPRE_V_DENOMINACION_PUCDto>? PreDenominacionPuc  { get; set; }
         public List<GetPreDenominacionPucResumenAnoDto>? PreDenominacionPucResumen { get; set; }
