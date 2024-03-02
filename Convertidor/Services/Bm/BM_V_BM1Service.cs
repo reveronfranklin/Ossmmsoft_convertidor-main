09ca57631a73dc9cb36df8ba9a4f48937d3d3d89
reveronfranklin <reveronfranklin@gmail.com>
2024-03-02 11:26:29 -0400

    Upadate repo

--- Convertidor/Services/Bm/BM_V_BM1Service.cs
+++ Convertidor/Services/Bm/BM_V_BM1Service.cs
@@ -25,255 +25,232 @@ namespace Convertidor.Services.Catastro
 
         public async Task<ResultDto<List<Bm1GetDto>>> GetAll()
         {
-           
-           
-            ResultDto<List<Bm1GetDto>> response = new ResultDto<List<Bm1GetDto>>(null);
-            try
-            {
-
-                //var bienesFoto =  _bmBienesFotoRepository.BienesConFoto();
-                
-                var result = await _repository.GetAll();
-          
-                    var lista = from s in result
-                                  group s by new
-                                  {
-                                      CodigoIcp=s.CODIGO_ICP,
-                                      UnidadTrabajo = s.UNIDAD_TRABAJO,
-                                      CodigoGrupo = s.CODIGO_GRUPO,
-                                      CodigoNivel1 = s.CODIGO_NIVEL1,
-                                      CodigoNivel2 = s.CODIGO_NIVEL2,
-                                      NumeroLote = s.NUMERO_LOTE,
-                                      Cantidad = s.CANTIDAD,
-                                      NumeroPlaca = s.NUMERO_PLACA,
-                                      Articulo = s.ARTICULO,
-                                      Especificacion = s.ESPECIFICACION,
-                                      Servicio = s.SERVICIO,
-                                      ResponsableBien = s.RESPONSABLE_BIEN,
-                                      CodigoBien = s.CODIGO_BIEN,
-                                      CodigoMovBien=s.CODIGO_MOV_BIEN,
-                                      FechaMovimiento=s.FECHA_MOVIMIENTO
-                 
-                                      
-                                  } into g
-                                  select new Bm1GetDto()
-                                  {
-
-                                        CodigoIcp = g.Key.CodigoIcp,
-                                      UnidadTrabajo = g.Key.UnidadTrabajo,
-                                      CodigoGrupo = g.Key.CodigoGrupo,
-                                      CodigoNivel1 = g.Key.CodigoNivel1,
-                                      CodigoNivel2 = g.Key.CodigoNivel2,
-                                      ConsecutivoPlaca=g.Key.NumeroPlaca,
-                                      NumeroLote = g.Key.NumeroLote,
-                                      Cantidad = g.Key.Cantidad,
-                                      NumeroPlaca = g.Key.CodigoGrupo + "-" + g.Key.CodigoNivel1 +"-" +  g.Key.CodigoNivel2 + "-"+g.Key.NumeroPlaca,
-                                      Articulo = g.Key.Articulo,
-                                      Especificacion = g.Key.Especificacion,   
-                                      Servicio = g.Key.Servicio,
-                                      ResponsableBien = g.Key.ResponsableBien,
-                                      CodigoBien = g.Key.CodigoBien,
-                                      CodigoMovBien = g.Key.CodigoMovBien,
-                                      FechaMovimiento=g.Key.FechaMovimiento
-
-                                  };
-                    
-                
-                       var listaExcel = from s in result
-                                  group s by new
-                                  {
-                                      UnidadTrabajo = s.UNIDAD_TRABAJO,
-                                      CodigoGrupo = s.CODIGO_GRUPO,
-                                      CodigoNivel1 = s.CODIGO_NIVEL1,
-                                      CodigoNivel2 = s.CODIGO_NIVEL2,
-                                      NumeroLote = s.NUMERO_LOTE,
-                                      Cantidad = s.CANTIDAD,
-                                      NumeroPlaca = s.NUMERO_PLACA,
-                                      Articulo = s.ARTICULO,
-                                      Especificacion = s.ESPECIFICACION,
-                                      Servicio = s.SERVICIO,
-                                      ResponsableBien = s.RESPONSABLE_BIEN,
-                                      CodigoBien = s.CODIGO_BIEN,
-                                      CodigoMovBien=s.CODIGO_MOV_BIEN
-                 
-                                      
-                                  } into g
-                                  select new Bm1ExcelGetDto()
-                                  {
-
-                                  
-                                      UnidadTrabajo = g.Key.UnidadTrabajo,
-                                    
-                                      Cantidad = g.Key.Cantidad,
-                                      NumeroPlaca = g.Key.CodigoGrupo + "-" + g.Key.CodigoNivel1 +"-" +  g.Key.CodigoNivel2 + "-"+g.Key.NumeroPlaca,
-                                      Articulo = g.Key.Articulo,
-                                      Especificacion = g.Key.Especificacion,   
-                                      Servicio = g.Key.Servicio,
-                                      ResponsableBien = g.Key.ResponsableBien,
-                                    
-
-                                  };
-                    ExcelMapper mapper = new ExcelMapper();
 
 
-                    var settings = _configuration.GetSection("Settings").Get<Settings>();
+        ResultDto<List<Bm1GetDto>> response = new ResultDto<List<Bm1GetDto>>(null);
+        try
+        {
 
-               
-                    var ruta = @settings.ExcelFiles;  //@"/Users/freveron/Documents/MM/App/full-version/public/ExcelFiles";
-                    var fileName = $"BM1.xlsx";
-                    string newFile = Path.Combine(Directory.GetCurrentDirectory(), ruta, fileName);
+        //var bienesFoto =  _bmBienesFotoRepository.BienesConFoto();
 
-                    var excelData = listaExcel.ToList();
-                    mapper.Save(newFile, excelData, $"BM1", true);
+        var result = await _repository.GetAll();
+
+        var lista = from s in result
+              group s by new
+              {
+                  CodigoIcp=s.CODIGO_ICP,
+                  UnidadTrabajo = s.UNIDAD_TRABAJO,
+                  CodigoGrupo = s.CODIGO_GRUPO,
+                  CodigoNivel1 = s.CODIGO_NIVEL1,
+                  CodigoNivel2 = s.CODIGO_NIVEL2,
+                  NumeroLote = s.NUMERO_LOTE,
+                  Cantidad = s.CANTIDAD,
+                  NumeroPlaca = s.NUMERO_PLACA,
+                  Articulo = s.ARTICULO,
+                  Especificacion = s.ESPECIFICACION,
+                  Servicio = s.SERVICIO,
+                  ResponsableBien = s.RESPONSABLE_BIEN,
+                  CodigoBien = s.CODIGO_BIEN,
+                  CodigoMovBien=s.CODIGO_MOV_BIEN,
+                  FechaMovimiento=s.FECHA_MOVIMIENTO
+
                   
-                    
-                    
-                response.Data = lista.OrderBy(x=> x.CodigoGrupo)
-                            .ThenBy(x=>x.CodigoNivel1)
-                            .ThenBy(x=>x.CodigoNivel2)
-                            .ThenBy(x=>x.ConsecutivoPlaca).ToList();
-                response.IsValid = true;
-                response.Message = "";
-                response.LinkData= $"/ExcelFiles/{fileName}";
-                return response;
-            }
-            catch (Exception ex)
-            {
-                response.Data = null;
-                response.IsValid = true;
-                response.Message = ex.InnerException.Message;
-                return response;
-            }
-           
-        }
-  public async Task<ResultDto<List<Bm1GetDto>>> GetAllByIcp(int codigoIcp)
-        {
-           
-           
-            ResultDto<List<Bm1GetDto>> response = new ResultDto<List<Bm1GetDto>>(null);
-            try
-            {
+              } into g
+              select new Bm1GetDto()
+              {
 
-                //var bienesFoto =  _bmBienesFotoRepository.BienesConFoto();
-                
-                var result = await _repository.GetAllByCodigoIcp(codigoIcp);
-          
-                    var lista = from s in result
-                                  group s by new
-                                  {
-                                      CodigoIcp=s.CODIGO_ICP,
-                                      UnidadTrabajo = s.UNIDAD_TRABAJO,
-                                      CodigoGrupo = s.CODIGO_GRUPO,
-                                      CodigoNivel1 = s.CODIGO_NIVEL1,
-                                      CodigoNivel2 = s.CODIGO_NIVEL2,
-                                      NumeroLote = s.NUMERO_LOTE,
-                                      Cantidad = s.CANTIDAD,
-                                      NumeroPlaca = s.NUMERO_PLACA,
-                                      Articulo = s.ARTICULO,
-                                      Especificacion = s.ESPECIFICACION,
-                                      Servicio = s.SERVICIO,
-                                      ResponsableBien = s.RESPONSABLE_BIEN,
-                                      CodigoBien = s.CODIGO_BIEN,
-                                      CodigoMovBien=s.CODIGO_MOV_BIEN,
-                                      FechaMovimiento=s.FECHA_MOVIMIENTO
-                 
-                                      
-                                  } into g
-                                  select new Bm1GetDto()
-                                  {
+                    CodigoIcp = g.Key.CodigoIcp,
+                  UnidadTrabajo = g.Key.UnidadTrabajo,
+                  CodigoGrupo = g.Key.CodigoGrupo,
+                  CodigoNivel1 = g.Key.CodigoNivel1,
+                  CodigoNivel2 = g.Key.CodigoNivel2,
+                  ConsecutivoPlaca=g.Key.NumeroPlaca,
+                  NumeroLote = g.Key.NumeroLote,
+                  Cantidad = g.Key.Cantidad,
+                  NumeroPlaca = g.Key.CodigoGrupo + "-" + g.Key.CodigoNivel1 +"-" +  g.Key.CodigoNivel2 + "-"+g.Key.NumeroPlaca,
+                  Articulo = g.Key.Articulo,
+                  Especificacion = g.Key.Especificacion,   
+                  Servicio = g.Key.Servicio,
+                  ResponsableBien = g.Key.ResponsableBien,
+                  CodigoBien = g.Key.CodigoBien,
+                  CodigoMovBien = g.Key.CodigoMovBien,
+                  FechaMovimiento=g.Key.FechaMovimiento
 
-                                        CodigoIcp = g.Key.CodigoIcp,
-                                      UnidadTrabajo = g.Key.UnidadTrabajo,
-                                      CodigoGrupo = g.Key.CodigoGrupo,
-                                      CodigoNivel1 = g.Key.CodigoNivel1,
-                                      CodigoNivel2 = g.Key.CodigoNivel2,
-                                      ConsecutivoPlaca=g.Key.NumeroPlaca,
-                                      NumeroLote = g.Key.NumeroLote,
-                                      Cantidad = g.Key.Cantidad,
-                                      NumeroPlaca = g.Key.CodigoGrupo + "-" + g.Key.CodigoNivel1 +"-" +  g.Key.CodigoNivel2 + "-"+g.Key.NumeroPlaca,
-                                      Articulo = g.Key.Articulo,
-                                      Especificacion = g.Key.Especificacion,   
-                                      Servicio = g.Key.Servicio,
-                                      ResponsableBien = g.Key.ResponsableBien,
-                                      CodigoBien = g.Key.CodigoBien,
-                                      CodigoMovBien = g.Key.CodigoMovBien,
-                                      FechaMovimiento=g.Key.FechaMovimiento
+              };
 
-                                  };
-                    
-                response.Data = lista
-                    .OrderBy(x=>x.CodigoBien)
-                    .ThenBy(x=> x.CodigoGrupo)
-                    .ThenBy(x=>x.CodigoNivel1)
-                    .ThenBy(x=>x.CodigoNivel2)
-                    .ThenBy(x=>x.ConsecutivoPlaca).ToList();
-                
-                response.IsValid = true;
-                response.Message = "";
-                response.LinkData= $"";
-                return response;
-            }
-            catch (Exception ex)
-            {
-                response.Data = null;
-                response.IsValid = true;
-                response.Message = ex.InnerException.Message;
-                return response;
-            }
-           
-        }
 
-        
-         public async Task<ResultDto<List<ICPGetDto>>> GetICP()
-        {
-           
-           
-            ResultDto<List<ICPGetDto>> response = new ResultDto<List<ICPGetDto>>(null);
-            try
-            {
+        var listaExcel = from s in result
+              group s by new
+              {
+                  UnidadTrabajo = s.UNIDAD_TRABAJO,
+                  CodigoGrupo = s.CODIGO_GRUPO,
+                  CodigoNivel1 = s.CODIGO_NIVEL1,
+                  CodigoNivel2 = s.CODIGO_NIVEL2,
+                  NumeroLote = s.NUMERO_LOTE,
+                  Cantidad = s.CANTIDAD,
+                  NumeroPlaca = s.NUMERO_PLACA,
+                  Articulo = s.ARTICULO,
+                  Especificacion = s.ESPECIFICACION,
+                  Servicio = s.SERVICIO,
+                  ResponsableBien = s.RESPONSABLE_BIEN,
+                  CodigoBien = s.CODIGO_BIEN,
+                  CodigoMovBien=s.CODIGO_MOV_BIEN
+
+                  
+              } into g
+              select new Bm1ExcelGetDto()
+              {
 
               
+                  UnidadTrabajo = g.Key.UnidadTrabajo,
                 
-                var result =  _repository.GetICP();
-          
-                   /* var lista = from s in result
-                                  group s by new
-                                  {
-                                      CodigoIcp=s.CODIGO_ICP,
-                                      UnidadTrabajo = s.UNIDAD_TRABAJO,
-                                    
-                 
-                                      
-                                  } into g
-                                  select new ICPGetDto()
-                                  {
+                  Cantidad = g.Key.Cantidad,
+                  NumeroPlaca = g.Key.CodigoGrupo + "-" + g.Key.CodigoNivel1 +"-" +  g.Key.CodigoNivel2 + "-"+g.Key.NumeroPlaca,
+                  Articulo = g.Key.Articulo,
+                  Especificacion = g.Key.Especificacion,   
+                  Servicio = g.Key.Servicio,
+                  ResponsableBien = g.Key.ResponsableBien,
+                
 
-                                        CodigoIcp = g.Key.CodigoIcp,
-                                        UnidadTrabajo = g.Key.UnidadTrabajo,
-                                   
+              };
+        ExcelMapper mapper = new ExcelMapper();
 
-                                  };*/
-                    
-            
 
-               
+        var settings = _configuration.GetSection("Settings").Get<Settings>();
+
+
+        var ruta = @settings.ExcelFiles;  //@"/Users/freveron/Documents/MM/App/full-version/public/ExcelFiles";
+        var fileName = $"BM1.xlsx";
+        string newFile = Path.Combine(Directory.GetCurrentDirectory(), ruta, fileName);
+
+        var excelData = listaExcel.ToList();
+        mapper.Save(newFile, excelData, $"BM1", true);
+
+
+
+        response.Data = lista.OrderBy(x=> x.CodigoGrupo)
+        .ThenBy(x=>x.CodigoNivel1)
+        .ThenBy(x=>x.CodigoNivel2)
+        .ThenBy(x=>x.ConsecutivoPlaca).ToList();
+        response.IsValid = true;
+        response.Message = "";
+        response.LinkData= $"/ExcelFiles/{fileName}";
+        return response;
+        }
+        catch (Exception ex)
+        {
+        response.Data = null;
+        response.IsValid = true;
+        response.Message = ex.InnerException.Message;
+        return response;
+        }
+
+        }
+        public async Task<ResultDto<List<Bm1GetDto>>> GetAllByIcp(int codigoIcp)
+        {
+
+
+        ResultDto<List<Bm1GetDto>> response = new ResultDto<List<Bm1GetDto>>(null);
+        try
+        {
+
+        //var bienesFoto =  _bmBienesFotoRepository.BienesConFoto();
+
+        var result = await _repository.GetAllByCodigoIcp(codigoIcp);
+
+        var lista = from s in result
+              group s by new
+              {
+                  CodigoIcp=s.CODIGO_ICP,
+                  UnidadTrabajo = s.UNIDAD_TRABAJO,
+                  CodigoGrupo = s.CODIGO_GRUPO,
+                  CodigoNivel1 = s.CODIGO_NIVEL1,
+                  CodigoNivel2 = s.CODIGO_NIVEL2,
+                  NumeroLote = s.NUMERO_LOTE,
+                  Cantidad = s.CANTIDAD,
+                  NumeroPlaca = s.NUMERO_PLACA,
+                  Articulo = s.ARTICULO,
+                  Especificacion = s.ESPECIFICACION,
+                  Servicio = s.SERVICIO,
+                  ResponsableBien = s.RESPONSABLE_BIEN,
+                  CodigoBien = s.CODIGO_BIEN,
+                  CodigoMovBien=s.CODIGO_MOV_BIEN,
+                  FechaMovimiento=s.FECHA_MOVIMIENTO
+
                   
-                    
-                    
-                response.Data = result.OrderBy(x=>x.CodigoIcp).ToList();
-                response.IsValid = true;
-                response.Message = "";
-                response.LinkData= $"";
-                return response;
-            }
-            catch (Exception ex)
-            {
-                response.Data = null;
-                response.IsValid = true;
-                response.Message = ex.InnerException.Message;
-                return response;
-            }
-           
+              } into g
+              select new Bm1GetDto()
+              {
+
+                    CodigoIcp = g.Key.CodigoIcp,
+                  UnidadTrabajo = g.Key.UnidadTrabajo,
+                  CodigoGrupo = g.Key.CodigoGrupo,
+                  CodigoNivel1 = g.Key.CodigoNivel1,
+                  CodigoNivel2 = g.Key.CodigoNivel2,
+                  ConsecutivoPlaca=g.Key.NumeroPlaca,
+                  NumeroLote = g.Key.NumeroLote,
+                  Cantidad = g.Key.Cantidad,
+                  NumeroPlaca = g.Key.CodigoGrupo + "-" + g.Key.CodigoNivel1 +"-" +  g.Key.CodigoNivel2 + "-"+g.Key.NumeroPlaca,
+                  Articulo = g.Key.Articulo,
+                  Especificacion = g.Key.Especificacion,   
+                  Servicio = g.Key.Servicio,
+                  ResponsableBien = g.Key.ResponsableBien,
+                  CodigoBien = g.Key.CodigoBien,
+                  CodigoMovBien = g.Key.CodigoMovBien,
+                  FechaMovimiento=g.Key.FechaMovimiento
+
+              };
+
+        response.Data = lista
+        .OrderBy(x=>x.CodigoBien)
+        .ThenBy(x=> x.CodigoGrupo)
+        .ThenBy(x=>x.CodigoNivel1)
+        .ThenBy(x=>x.CodigoNivel2)
+        .ThenBy(x=>x.ConsecutivoPlaca).ToList();
+
+        response.IsValid = true;
+        response.Message = "";
+        response.LinkData= $"";
+        return response;
+        }
+        catch (Exception ex)
+        {
+        response.Data = null;
+        response.IsValid = true;
+        response.Message = ex.InnerException.Message;
+        return response;
+        }
+
+        }
+
+
+        public async Task<ResultDto<List<ICPGetDto>>> GetICP()
+        {
+
+
+        ResultDto<List<ICPGetDto>> response = new ResultDto<List<ICPGetDto>>(null);
+        try
+        {
+
+
+
+        var result =  _repository.GetICP();
+
+
+        response.Data = result.OrderBy(x=>x.CodigoIcp).ToList();
+        response.IsValid = true;
+        response.Message = "";
+        response.LinkData= $"";
+        return response;
+        }
+        catch (Exception ex)
+        {
+        response.Data = null;
+        response.IsValid = true;
+        response.Message = ex.InnerException.Message;
+        return response;
+        }
+
         }
         
         
