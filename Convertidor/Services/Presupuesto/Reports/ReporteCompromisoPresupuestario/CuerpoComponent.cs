9b806172ab0d7ee46a99924eb8689aacb40984f2
OmarJNadal <omarles4568@gmail.com>
2024-08-06 16:55:32 -0400

    OSS-17 ReporteCompromisoPresupuestario refactor ReporteCompromisoPresupuestarioDto Add prop PucCompromisos,CuerpoComponent refactor Table Layouts,content  ,ReporteCompromisoPresupuestarioService refactor GenerateDataCuerpoDto,  refactor FilterPreCompromisosDto

--- Convertidor/Services/Presupuesto/Reports/ReporteCompromisoPresupuestario/CuerpoComponent.cs
+++ Convertidor/Services/Presupuesto/Reports/ReporteCompromisoPresupuestario/CuerpoComponent.cs
@@ -30,17 +30,17 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
             formato.NumberDecimalSeparator = ",";
             formato.NumberDecimalDigits = 2;
 
-          
+
             container.Table(table =>
             {
-               
-                
+
+
 
                 table.ExtendLastCellsToTableBottom();
 
                 table.ColumnsDefinition(columns =>
                 {
-                    
+
                     columns.ConstantColumn(320);
                     columns.RelativeColumn();
                     columns.RelativeColumn();
@@ -49,7 +49,7 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
                     columns.RelativeColumn();
                 });
 
-                
+
 
                 table.Header(header =>
                 {
@@ -59,27 +59,26 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
                     header.Cell().ColumnSpan(6).Row(row =>
                       {
 
-                        row.ConstantItem(40).Border(1).AlignCenter().Element(CellStyle).Text("CANTIDAD").FontSize(7).Bold();
-                        row.ConstantItem(50).Border(1).AlignCenter().Element(CellStyle).Text("   UNIDAD\n"+ "DE MEDIDA").FontSize(7).Bold();
-                        row.RelativeItem(3).Border(1).AlignCenter().Element(CellStyle).PaddingLeft(50).Text("DESCRIPCION").FontSize(7).Bold();
-                        row.ConstantItem(60).Border(1).AlignCenter().Element(CellStyle).Text("   PRECIO\n"+ "UNITARIO").FontSize(7).Bold();
+                          row.ConstantItem(40).Border(1).AlignCenter().Element(CellStyle).Text("CANTIDAD").FontSize(7).Bold();
+                          row.ConstantItem(50).Border(1).AlignCenter().Element(CellStyle).Text("   UNIDAD\n" + "DE MEDIDA").FontSize(7).Bold();
+                          row.RelativeItem(3).Border(1).AlignCenter().Element(CellStyle).PaddingLeft(50).Text("DESCRIPCION").FontSize(7).Bold();
+                          row.ConstantItem(60).Border(1).AlignCenter().Element(CellStyle).Text("   PRECIO\n" + "UNITARIO").FontSize(7).Bold();
                           row.ConstantItem(70).Column(col =>
                           {
                               col.Item().Border(1).AlignCenter().Element(CellStyle).Text("TOTAL").FontSize(7).Bold();
                               col.Item().Border(1).AlignCenter().Element(CellStyle).Text("BOLIVARES").FontSize(7).Bold();
                           });
-                        
 
 
-                    });
+
+                      });
 
                 });
 
-              
                 foreach (var item in ModelCuerpo)
                 {
 
-                    
+
                     table.Cell().ColumnSpan(6).Row(row =>
                     {
                         row.ConstantItem(40).BorderVertical(1).AlignCenter().PaddingRight(3).PaddingTop(5).Element(CellStyle).Text(item.Cantidad).FontSize(7);
@@ -93,57 +92,79 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
 
                     });
 
-                    
+
+
 
                 }
 
-                table.Cell();
+          
 
-                table.Cell().Column(col =>
+
+                foreach (var item in ModelCuerpo)
                 {
                     
-                    col.Item().Text("SE-PR-SP-PY-AC").FontSize(7).Bold().Underline();
-                    col.Item().Text(ModelEncabezado.CodigoIcpConcat).FontSize(7);
-                    col.Item().Text(ModelEncabezado.CodigoIcpConcat).FontSize(7);
+                    var pucCompromisos = item.PucCompromisos;
 
-                });
+                    foreach (var itemPuc in pucCompromisos.OrderByDescending(x => x.CodigoDetalleCompromiso))
+                    {
+                        
+                        table.Cell().ColumnSpan(3).Row(row =>
+                        {
 
-                table.Cell().Column(async col =>
-                {
-                   
-                    var pucConcatOrdinario = ModelCuerpo.ElementAt(7);
-                    var pucConcatTraspaso = ModelCuerpo.ElementAt(0);
-                    col.Item().Text("GR-PA-GE-ES-SE-EX").FontSize(7).Bold().Underline();
-                    
-                    col.Item().Text(pucConcatTraspaso.CodigoPucConcat).FontSize(7);
-                    col.Item().Text(pucConcatOrdinario.CodigoPucConcat).FontSize(7);
-                });
+                        row.ConstantItem(40).BorderVertical(1).Text("");
+                        row.ConstantItem(50).BorderVertical(1).Text("");
 
-                table.Cell().Column(col =>
-                {
-                    var financiado = ModelCuerpo.ElementAt(0);
-                    col.Item().Text("Financiado Por").FontSize(7).Bold().Underline();
-                    col.Item().AlignCenter().Text(financiado.DescripcionFinanciado).FontSize(7);
-                    //if(financiado.DescripcionFinanciado.Contains("TRASPASO PRESUPUESTARIO")) 
-                    //{
-                    //    col.Item().Text(financiado.DescripcionFinanciado= "TRASPASO\nPRESUPUESTARIO").FontSize(7);
-                    //}
-                    
+                        row.RelativeItem().PaddingLeft(5).ShowOnce().Column(col =>
+                        {
 
-                });
+                            col.Item().Text("SE-PR-SP-PY-AC").FontSize(7).Bold().Underline();
+                            col.Item().Text(itemPuc.CodigoIcpConcat).FontSize(7);
+                            col.Item().Text(itemPuc.CodigoIcpConcat).FontSize(7);
 
-                table.Cell().Column(col =>
-                {
-                    var montoIva = ModelCuerpo.Sum(x => x.MontoImpuesto);
-                    var monto = montoIva.ToString("N", formato);
-                    col.Item().Text("Monto").FontSize(7).Bold().Underline();
-                    var montoTraspaso = ModelCuerpo.Sum(x => x.Monto);
-                    var montoPresupuestario = montoTraspaso.ToString("N", formato);
-                    col.Item().Text(montoPresupuestario).FontSize(7);
-                    col.Item().Text(monto).FontSize(7);
-                   
+                        });
 
-                });
+                        row.RelativeItem().ShowOnce().Column(async col =>
+                        {
+                                
+                                var ivaPucConcat = itemPuc.CodigoPucConcat;
+                                var montoIva = itemPuc.Monto;
+
+                                col.Item().Text("GR-PA-GE-ES-SE-EX").FontSize(7).Bold().Underline();
+                                col.Item().Text(itemPuc.CodigoPucConcat).FontSize(7);
+                                col.Item().Text(ivaPucConcat).FontSize(7);
+                            
+                        });
+
+                        row.RelativeItem().ShowOnce().Column(col =>
+                        {
+
+                            col.Item().Text("Financiado Por").FontSize(7).Bold().Underline();
+                            col.Item().AlignCenter().Text(itemPuc.DescripcionFinanciado).FontSize(7);
+
+                        });
+
+
+                        row.RelativeItem().ShowOnce().Column(col =>
+                        {
+                            col.Item().Text("Monto").FontSize(7).Bold().Underline();
+                            
+                                var montoTraspaso = pucCompromisos.Sum(x => x.Monto);
+                                var montoPresupuestario = montoTraspaso.ToString("N", formato);
+                                col.Item().Text(montoPresupuestario).FontSize(7);
+                            
+                                var montoIva = pucCompromisos.Sum(x => x.Monto);
+                                var monto = montoIva.ToString("N", formato);
+                                col.Item().Text(monto).FontSize(7);
+
+                            
+                        });
+
+                            row.ConstantItem(60).BorderVertical(1).Text("");
+                            row.ConstantItem(70).BorderVertical(1).Text("");
+
+                        });
+                    }
+                }
                 table.Footer(footer =>
                 {
 
@@ -151,9 +172,9 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
                     footer.Cell().ColumnSpan(6).ExtendHorizontal().BorderVertical(1).BorderBottom(1).BorderTop(1).PaddingVertical(2).Row(row =>
                     {
                         row.RelativeItem(5).PaddingLeft(10).PaddingRight(5).AlignLeft().Text($"MONTO TOTAL EN LETRA :\n{ModelEncabezado.MontoEnLetras.ToUpper()}").FontSize(8).Bold();
-                        
-                       
-                        
+
+
+
                         row.ConstantItem(70).AlignRight().AlignBottom().PaddingRight(2).Text("TOTAL").FontSize(8).Bold();
 
                         var bolivares = ModelCuerpo.Sum(x => x.TotalBolivares);
@@ -165,20 +186,21 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
                         var totales = total.ToString("N", formato);
                         row.ConstantItem(70).BorderLeft(1).AlignRight().Column(col =>
                         {
-                           
-                           col.Item().BorderBottom(1).ExtendHorizontal().PaddingVertical(20);
-                           col.Item().ExtendHorizontal().AlignRight().AlignBottom().Element(CellStyle).PaddingBottom(-1).PaddingRight(3).Text(totalBolivares).FontSize(7);
+
+                            col.Item().BorderBottom(1).ExtendHorizontal().PaddingVertical(20);
+                            col.Item().ExtendHorizontal().AlignRight().AlignBottom().Element(CellStyle).PaddingBottom(-1).PaddingRight(3).Text(totalBolivares).FontSize(7);
                         });
-                     
+
                     });
 
 
-         
-                    
+
+
+
 
                     footer.Cell().ColumnSpan(6).Column(col =>
                     {
-                        col.Item().BorderVertical(1).BorderTop(1).PaddingLeft(3).Text("MOTIVO  :" ).FontSize(8).Bold();
+                        col.Item().BorderVertical(1).BorderTop(1).PaddingLeft(3).Text("MOTIVO  :").FontSize(8).Bold();
                         col.Item().BorderVertical(1).PaddingVertical(20).PaddingLeft(3).Text(ModelEncabezado.Motivo).FontSize(7);
                     });
 
@@ -190,14 +212,14 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
                             col.Item().BorderVertical(1).Text($"{ModelEncabezado.Firmante}").FontSize(7);
                             col.Item().BorderVertical(1).BorderBottom(1).PaddingLeft(4).PaddingVertical(4).Text($"FIRMA : ________________________________________     ").FontSize(8).Bold();
                         });
-                       
-                        
+
+
 
                     });
 
                     footer.Cell().ColumnSpan(5).BorderVertical(1).BorderBottom(1).BorderTop(1).AlignBottom().AlignCenter().Padding(3).PaddingLeft(8).PaddingBottom(5).Text($"DIRECCION DE PLANIFICACION Y PRESUPUESTO").FontSize(8).Bold();
 
-                    
+
                 });
 
                 table.Cell().ColumnSpan(4).Column(col =>
@@ -205,36 +227,36 @@ namespace Convertidor.Services.Presupuesto.ReporteCompromisoPresupuestario
                     col.Item().PageBreak();
                 });
 
-               
-                    table.ExtendLastCellsToTableBottom();
 
-                    table.Cell().ColumnSpan(6).Border(1).Text("");
-                    
-                    table.Cell().ColumnSpan(6).Row(row =>
+                table.ExtendLastCellsToTableBottom();
+
+                table.Cell().ColumnSpan(6).Border(1).Text("");
+
+                table.Cell().ColumnSpan(6).Row(row =>
+                {
+
+                    row.ConstantItem(40).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).Text("").FontSize(7).Bold();
+                    row.ConstantItem(50).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).Text(" ").FontSize(7).Bold();
+                    row.RelativeItem(3).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).PaddingLeft(50).Text("").FontSize(7).Bold();
+                    row.ConstantItem(60).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).Text(" ").FontSize(7).Bold();
+                    row.ConstantItem(70).BorderVertical(1).PaddingVertical(20).Column(col =>
                     {
-
-                        row.ConstantItem(40).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).Text("").FontSize(7).Bold();
-                        row.ConstantItem(50).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).Text(" ").FontSize(7).Bold();
-                        row.RelativeItem(3).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).PaddingLeft(50).Text("").FontSize(7).Bold();
-                        row.ConstantItem(60).Border(1).AlignCenter().PaddingVertical(20).Element(CellStyle).Text(" ").FontSize(7).Bold();
-                        row.ConstantItem(70).BorderVertical(1).PaddingVertical(20).Column(col =>
-                        {
-                            
-                            
-                        });
-
 
 
                     });
 
+
+
+                });
+
             });
-            
 
-   
 
-            
+
+
+
+     
         }
-
        
     }
 }
