f4b29096f6255e1c048d171504e1702afaf8fe1a
OmarJNadal <omarles4568@gmail.com>
2024-08-15 13:59:42 -0400

    OSS-58 Crud CatTitulos Add Methods GetItems, WriteStrings,GetTreeTitulosRespaldo in CatTitulosService

--- Convertidor/Services/Catastro/CatTitulosService.cs
+++ Convertidor/Services/Catastro/CatTitulosService.cs
@@ -149,6 +149,161 @@ namespace Convertidor.Services.Catastro
             return result;
         }
 
+        public async Task<List<Item>> GetItems()
+        {
+            List<Item> result = new List<Item>();
+            var descriptivas = await _repository.GetAll();
+
+            foreach (var item in descriptivas)
+            {
+                Item itenNew = new Item();
+                itenNew.Id = item.TITULO_ID;
+
+                itenNew.ParentId = (int)item.TITULO_FK_ID;
+                var patchEvaluado = item.TITULO;
+
+                itenNew.Name = patchEvaluado;
+                itenNew.Denominacion = item.TITULO;
+                if (item.TITULO == null) item.TITULO = "";
+                itenNew.Descripcion = item.TITULO;
+                result.Add(itenNew);
+
+            }
+
+            return result;
+        }
+
+        public async Task<List<string>> WriteStrings()
+        {
+
+            List<string> result = new List<string>();
+            List<Item> items = await GetItems();
+
+
+            IEnumerable<string> resultingStrings = from parent in items.Where(x => x.ParentId == 0)
+                                                   join child in items on parent.Id equals child.ParentId
+                                                   //join grandChild in items on child.Id equals grandChild.ParentId
+
+
+                                                   select string.Format("{0}:{1}:{2}:{3}:{4}:{5}", parent.Id.ToString() + "-" + parent.Name, child.Id.ToString() + "-" + child.Name, child.Id, child.ParentId, child.Denominacion, child.Descripcion);
+
+
+            var lista = resultingStrings.ToList();
+
+            foreach (var item in lista)
+            {
+                var search = result.Where(x => x == item).FirstOrDefault();
+                if (search == null)
+                {
+                    result.Add(item);
+                }
+
+
+            }
+
+            IEnumerable<string> resultingAddStrings = from parent in items
+                                                          //join child in items on parent.Id equals child.ParentId
+                                                          //join grandChild in items on child.Id equals grandChild.ParentId
+
+
+                                                      select string.Format("{0}:{1}:{2}:{3}:{4}:{5}", parent.Id.ToString() + "-" + parent.Name, parent.Id.ToString() + "-" + parent.Name, parent.Id, parent.ParentId, parent.Denominacion, parent.Descripcion);
+
+
+
+            lista = resultingAddStrings.ToList();
+            foreach (var item in lista)
+            {
+                var search = result.Where(x => x == item).FirstOrDefault();
+                if (search == null)
+                {
+                    result.Add(item);
+                }
+
+
+
+            }
+
+
+            return result;
+        }
+
+        public async Task<ResultDto<List<TreePUC>>> GetTreeTitulosRespaldo()
+        {
+
+
+
+            List<TreePUC> listTreePUC = new List<TreePUC>();
+            ResultDto<List<TreePUC>> result = new ResultDto<List<TreePUC>>(null);
+            try
+            {
+
+
+                var treePuc = await WriteStrings();
+
+                foreach (var item in treePuc)
+                {
+                    var arraIcp = item.Split(":");
+                    var id = arraIcp[2];
+                    var parentId = arraIcp[3];
+                    List<string> icpString = new List<string>();
+                    var match = icpString.FirstOrDefault(stringToCheck => stringToCheck.Contains(arraIcp[0]));
+                    if (match == null)
+                        icpString.Add(arraIcp[0]);
+
+                    match = icpString.FirstOrDefault(stringToCheck => stringToCheck.Contains(arraIcp[1]));
+                    if (match == null)
+                        icpString.Add(arraIcp[1]);
+
+                    /*match = icpString.FirstOrDefault(stringToCheck => stringToCheck.Contains(arraIcp[2]));
+                    if (match == null)
+                        icpString.Add(arraIcp[2]);
+                    match = icpString.FirstOrDefault(stringToCheck => stringToCheck.Contains(arraIcp[3]));
+                    if (match == null)
+                        icpString.Add(arraIcp[3]);*/
+
+
+
+
+                    TreePUC treePUC = new TreePUC();
+                    treePUC.Path = icpString;
+                    treePUC.Id = Int32.Parse(id);
+                    treePUC.Denominacion = arraIcp[4];
+                    treePUC.Descripcion = arraIcp[5];
+                    var search = listTreePUC.Where(x => x.Id == treePUC.Id).FirstOrDefault();
+                    if (search == null)
+                    {
+                        listTreePUC.Add(treePUC);
+                    }
+
+
+
+
+                }
+
+                result.Data = listTreePUC.OrderBy(x => x.Id).ToList();
+                result.IsValid = true;
+                result.Message = $"";
+                return result;
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = ex.Message;
+                return result;
+
+            }
+
+
+
+        }
+
         public async Task<ResultDto<CatTitulosResponseDto>> Create(CatTitulosUpdateDto dto)
         {
 
