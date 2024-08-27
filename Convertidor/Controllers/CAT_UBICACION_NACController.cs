1e3243a2656dd7f18b15793adfc80593f2427e1e
OmarJNadal <omarles4568@gmail.com>
2024-08-27 15:02:19 -0400

    OSS-66  Crud CAT_CONTROL_PARCELAS Add CatControlParcelasUpdateDto, OSS-105 Crud CAT_UBICACION_NAC Add methods GetPaises,GetEstados,GetMunicipios,GetCiudades,GetParroquias,GetSectores,GetUrbanizaciones,GetPais, GetEstado,GetMunicipio,GetCiudad,GetParroquia,GetSector,GetUrbanizacion in CAT_UBICACION_NACRepository, Add methods GetPaises,GetEstados,GetMunicipios,GetCiudades,GetParroquias,Getsectores,GetUrbanizaciones,GetPais,GetEstado,GetMunicipio,GetCiudad,GetParroquia,GetSector,GetUrbanizacion in CAT_UBICACION_NACService, Add endpoints GetPaises,GetEstados in CAT_UBICACION_NACController

---
+++ Convertidor/Controllers/CAT_UBICACION_NACController.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Convertidor.Dtos.Sis;
+using Convertidor.Services.Sis;
+using Convertidor.Data.Interfaces.Catastro;
+
+namespace Convertidor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class CAT_UBICACION_NACController : ControllerBase
+    {
+       
+        private readonly ICAT_UBICACION_NACRepository _service;
+
+        public CAT_UBICACION_NACController(ICAT_UBICACION_NACRepository service)
+        {
+
+            _service = service;
+           
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> GetPaises(FilterClave filter)
+        {
+                var result = await _service.GetPaises();
+            return Ok(result);
+        }
+        
+        
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> GetEstados(FilterClave filter)
+        {
+            var result = await _service.GetEstados();
+            return Ok(result);
+        }
+      
+    }
+}
