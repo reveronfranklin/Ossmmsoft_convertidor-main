d133ec8169d0c8f06eec686effcb6c378c6808ab
OmarJNadal <omarles4568@gmail.com>
2024-08-28 10:24:18 -0400

    OSS-66 Crud CAT_CONTROL_PARCELAS Add method GetNextKey, Add in CatControlParcelasRepository, Add CatControlParcelasUpdateDto, Add method Create in CatControlParcelasService, Add endpoint Create in CatControlParcelasController

--- Convertidor/Data/Repository/Catastro/CatControlParcelasRepository.cs
+++ Convertidor/Data/Repository/Catastro/CatControlParcelasRepository.cs
@@ -28,5 +28,66 @@ namespace Convertidor.Data.Repository.Catastro
             }
 
         }
+
+        public async Task<ResultDto<CAT_CONTROL_PARCELAS>> Add(CAT_CONTROL_PARCELAS entity)
+        {
+            ResultDto<CAT_CONTROL_PARCELAS> result = new ResultDto<CAT_CONTROL_PARCELAS>(null);
+            try
+            {
+
+
+
+                await _context.CAT_CONTROL_PARCELAS.AddAsync(entity);
+                await _context.SaveChangesAsync();
+
+
+                result.Data = entity;
+                result.IsValid = true;
+                result.Message = "";
+                return result;
+
+
+            }
+            catch (Exception ex)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = ex.Message;
+                return result;
+            }
+
+
+
+        }
+
+        public async Task<int> GetNextKey()
+        {
+            try
+            {
+                int result = 0;
+                var last = await _context.CAT_CONTROL_PARCELAS.DefaultIfEmpty()
+                    .OrderByDescending(x => x.CODIGO_CONTROL_PARCELA)
+                    .FirstOrDefaultAsync();
+                if (last == null)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    result = last.CODIGO_CONTROL_PARCELA + 1;
+                }
+
+                return (int)result!;
+
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return 0;
+            }
+
+
+
+        }
     }
 }
