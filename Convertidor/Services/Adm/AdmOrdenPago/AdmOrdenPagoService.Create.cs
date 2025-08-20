11199c3aab5bbe2aee16553b96d98156d70c62d8
reveronfranklin <reveronfranklin@gmail.com>
2025-08-20 10:29:23 -0400

    Refactor Reconstruccion De retenciones

--- Convertidor/Services/Adm/AdmOrdenPago/AdmOrdenPagoService.Create.cs
+++ Convertidor/Services/Adm/AdmOrdenPago/AdmOrdenPagoService.Create.cs
@@ -3,146 +3,143 @@ using Convertidor.Dtos.Adm;
 
 namespace Convertidor.Services.Adm.AdmOrdenPago;
 
-public partial class AdmOrdenPagoService 
+public partial class AdmOrdenPagoService
 {
-            public async Task<ResultDto<AdmOrdenPagoResponseDto>> Create(AdmOrdenPagoUpdateDto dto)
+    public async Task<ResultDto<AdmOrdenPagoResponseDto>> Create(AdmOrdenPagoUpdateDto dto)
+    {
+        ResultDto<AdmOrdenPagoResponseDto> result = new ResultDto<AdmOrdenPagoResponseDto>(null);
+        try
         {
-            ResultDto<AdmOrdenPagoResponseDto> result = new ResultDto<AdmOrdenPagoResponseDto>(null);
-            try
+            var conectado = await _sisUsuarioRepository.GetConectado();
+
+            var codigoOrdenPago = await _repository.GetCodigoOrdenPago((int)dto.CodigoOrdenPago);
+            if (codigoOrdenPago != null)
             {
-                var conectado = await _sisUsuarioRepository.GetConectado();
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Codigo  existe";
+                return result;
+            }
 
-                var codigoOrdenPago = await _repository.GetCodigoOrdenPago((int)dto.CodigoOrdenPago);
-                if (codigoOrdenPago != null)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Codigo  existe";
-                    return result;
-                }
-              
-                var compromiso = await _preCompromisosService.GetByCompromiso(dto.CodigoCompromiso);
-                if (compromiso == null)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Compromiso Invalido";
-                    return result;
-                }
+            var compromiso = await _preCompromisosService.GetByCompromiso(dto.CodigoCompromiso);
+            if (compromiso == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Compromiso Invalido";
+                return result;
+            }
 
-                
-                if (dto.FechaOrdenPago == null)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Fecha Orden Pago Invalida";
-                    return result;
-                }
-                var tipoOrdenPagoId = await _admDescriptivaRepository.GetByCodigo(dto.TipoOrdenPagoId);
-                if (tipoOrdenPagoId==null)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Tipo Orden Pago Invalido";
-                    return result;
-                }
-                
 
-                if (dto.CantidadPago <=0)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Cantidad Pago Invalida";
-                    return result;
-                }
+            if (dto.FechaOrdenPago == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Fecha Orden Pago Invalida";
+                return result;
+            }
 
-             
-                var frecuenciaPagoId = await _admDescriptivaRepository.GetByCodigo(dto.FrecuenciaPagoId);
-                if (frecuenciaPagoId==null)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Frecuencia Pago Invalido";
-                    return result;
-                }
+            var tipoOrdenPagoId = await _admDescriptivaRepository.GetByCodigo(dto.TipoOrdenPagoId);
+            if (tipoOrdenPagoId == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Tipo Orden Pago Invalido";
+                return result;
+            }
 
-                var tipoPagoId = await _admDescriptivaRepository.GetByCodigo(dto.TipoPagoId);
-                if (tipoPagoId==null)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Tipo Pago Id Invalido";
-                    return result;
-                }
 
-             
+            if (dto.CantidadPago <= 0)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Cantidad Pago Invalida";
+                return result;
+            }
 
-                if (dto.Motivo == null || dto.Motivo.Length<2)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Motivo Invalido";
-                    return result;
-                }
-             
 
-                if (dto.CodigoPresupuesto < 0)
-                {
+            var frecuenciaPagoId = await _admDescriptivaRepository.GetByCodigo(dto.FrecuenciaPagoId);
+            if (frecuenciaPagoId == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Frecuencia Pago Invalido";
+                return result;
+            }
 
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Codigo Presupuesto Invalido";
-                    return result;
-                }
+            var tipoPagoId = await _admDescriptivaRepository.GetByCodigo(dto.TipoPagoId);
+            if (tipoPagoId == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Tipo Pago Id Invalido";
+                return result;
+            }
 
-                var presupuesto = await _prePresupuestosRepository.GetByCodigo(conectado.Empresa, dto.CodigoPresupuesto);
-                if (presupuesto==null)
-                {
 
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Codigo Presupuesto Invalido";
-                    return result;
-                }
-                
-               
-           
+            if (dto.Motivo == null || dto.Motivo.Length < 2)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Motivo Invalido";
+                return result;
+            }
 
-                if (dto.NumeroComprobante < 0)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Numero comprobante Invalido";
-                    return result;
-                }
-                if (dto.NumeroComprobante > 0 && dto.FechaComprobante == null)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Fecha comprobante Invalida";
-                    return result;
-                }
 
-                if (dto.NumeroComprobante2 < 0)
-                {
-                    result.Data = null;
-                    result.IsValid = false;
-                    result.Message = "Numero comprobante2 Invalido";
-                    return result;
-                }
-                
-             
+            if (dto.CodigoPresupuesto < 0)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Codigo Presupuesto Invalido";
+                return result;
+            }
 
-               var compromisoPendiente= await  _admCompromisosPendientesRepository.GetCompromisosPendientesPorCodigoCompromiso(compromiso
+            var presupuesto = await _prePresupuestosRepository.GetByCodigo(conectado.Empresa, dto.CodigoPresupuesto);
+            if (presupuesto == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Codigo Presupuesto Invalido";
+                return result;
+            }
+
+
+            if (dto.NumeroComprobante < 0)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Numero comprobante Invalido";
+                return result;
+            }
+
+            if (dto.NumeroComprobante > 0 && dto.FechaComprobante == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Fecha comprobante Invalida";
+                return result;
+            }
+
+            if (dto.NumeroComprobante2 < 0)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "Numero comprobante2 Invalido";
+                return result;
+            }
+
+
+            var compromisoPendiente =
+                await _admCompromisosPendientesRepository.GetCompromisosPendientesPorCodigoCompromiso(compromiso
                     .CodigoCompromiso);
-               if (compromisoPendiente==null)
-               {
-                   result.Data = null;
-                   result.IsValid = false;
-                   result.Message = "COMPROMISO NO ESTA PENDIENTE";
-                   return result;
-               } 
-               
+            if (compromisoPendiente == null)
+            {
+                result.Data = null;
+                result.IsValid = false;
+                result.Message = "COMPROMISO NO ESTA PENDIENTE";
+                return result;
+            }
+
 
             ADM_ORDEN_PAGO entity = new ADM_ORDEN_PAGO();
             entity.CODIGO_ORDEN_PAGO = await _repository.GetNextKey();
@@ -162,7 +159,7 @@ public partial class AdmOrdenPagoService
             {
                 entity.FECHA_COMPROBANTE = (DateTime)dto.FechaComprobante;
             }
-          
+
             entity.NUMERO_COMPROBANTE2 = dto.NumeroComprobante2;
             entity.NUMERO_COMPROBANTE3 = dto.NumeroComprobante3;
             entity.NUMERO_COMPROBANTE4 = dto.NumeroComprobante4;
@@ -173,7 +170,7 @@ public partial class AdmOrdenPagoService
             {
                 entity.CON_FACTURA = 1;
             }
-            
+
             entity.CODIGO_EMPRESA = conectado.Empresa;
             entity.USUARIO_INS = conectado.Usuario;
             entity.FECHA_INS = DateTime.Now;
@@ -181,10 +178,9 @@ public partial class AdmOrdenPagoService
             var created = await _repository.Add(entity);
             if (created.IsValid && created.Data != null)
             {
-                
                 //CREAR LOS PUC a parir del compromiso
-                await CrearPucOrdenPagoDesdeCompromiso(dto.CodigoCompromiso,created.Data.CODIGO_ORDEN_PAGO);
-                
+                await CrearPucOrdenPagoDesdeCompromiso(dto.CodigoCompromiso, created.Data.CODIGO_ORDEN_PAGO);
+
                 //CREAMOS EL COMPROMISO DE LA ORDEN DE PAGO
                 AdmCompromisoOpUpdateDto compromisoOp = new AdmCompromisoOpUpdateDto();
                 compromisoOp.CodigoCompromisoOp = 0;
@@ -192,45 +188,40 @@ public partial class AdmOrdenPagoService
                 compromisoOp.CodigoPresupuesto = compromiso.CodigoPresupuesto;
                 compromisoOp.CodigoOrdenPago = created.Data.CODIGO_ORDEN_PAGO;
                 compromisoOp.CodigoValContrato = 0;
-                compromisoOp.OrigenCompromisoId = compromisoPendiente.ORIGEN_COMPROMISO_ID; 
-                compromisoOp.CodigoIdentificador = compromiso.CodigoCompromiso; 
+                compromisoOp.OrigenCompromisoId = compromisoPendiente.ORIGEN_COMPROMISO_ID;
+                compromisoOp.CodigoIdentificador = compromiso.CodigoCompromiso;
                 var compromisOpCreated = await _admCompromisoOpService.Create(compromisoOp);
-                
-               
-                await _prePresupuestosRepository.RecalcularSaldo(created.Data.CODIGO_PRESUPUESTO);
-             
-                
-                var descriptivas = await _admDescriptivaRepository.GetAll();
-                var proveedores = await _admProveedoresRepository.GetByCodigo(compromisoOp.CodigoProveedor );
-                var resultDto = await MapOrdenPagoDto(created.Data,descriptivas,proveedores);
 
-                result.Data = resultDto; 
+
+                await _prePresupuestosRepository.RecalcularSaldo(created.Data.CODIGO_PRESUPUESTO);
+
+
+                var descriptivas = await _admDescriptivaRepository.GetAll();
+                var proveedores = await _admProveedoresRepository.GetByCodigo(compromisoOp.CodigoProveedor);
+                var resultDto = await MapOrdenPagoDto(created.Data, descriptivas, proveedores);
+
+                result.Data = resultDto;
                 //resultDto;
                 result.IsValid = true;
                 result.Message = "";
             }
             else
             {
-
                 result.Data = null;
                 result.IsValid = created.IsValid;
                 result.Message = created.Message;
             }
 
             return result;
-
-
-            }
-            catch (Exception ex)
-            {
-                result.Data = null;
-                result.IsValid = false;
-                result.Message = ex.Message;
-            }
-
-
-
-            return result;
+        }
+        catch (Exception ex)
+        {
+            result.Data = null;
+            result.IsValid = false;
+            result.Message = ex.Message;
         }
 
+
+        return result;
+    }
 }
