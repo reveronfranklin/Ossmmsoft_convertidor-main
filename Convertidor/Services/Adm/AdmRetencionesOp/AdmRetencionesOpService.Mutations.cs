d7c37ef065bf0b98bcc4f6bb431132db5d2fc103
reveronfranklin <reveronfranklin@gmail.com>
2025-09-30 19:03:04 -0400

    Validar totales retenciones en add retiones op

--- Convertidor/Services/Adm/AdmRetencionesOp/AdmRetencionesOpService.Mutations.cs
+++ Convertidor/Services/Adm/AdmRetencionesOp/AdmRetencionesOpService.Mutations.cs
@@ -216,7 +216,11 @@ namespace Convertidor.Services.Adm.AdmRetencionesOp
             var retenciones = await _repository.GetByOrdenPago(codigoOrdenPago);
             if (retenciones != null && retenciones.Count() > 0)
             {
-                totalRetenciones = (decimal)retenciones.Sum(t => t.MONTO_RETENCION);
+                var descriptivaIva = await _admDescriptivaRepository.GetByCodigoDescriptivaTexto("IVA");
+                
+                totalRetenciones = (decimal)retenciones
+                    .Where(X=>X.TIPO_RETENCION_ID!=descriptivaIva.DESCRIPCION_ID)
+                    .Sum(t => t.MONTO_RETENCION);
 
             }
             
