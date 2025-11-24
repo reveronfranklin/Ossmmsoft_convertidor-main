3934f64ea057c962835fa0195688c5b8182c8db4
reveronfranklin <reveronfranklin@gmail.com>
2025-11-24 10:14:42 -0400

    Add endpoint Create,Update RhDirecciones

--- Convertidor/Dtos/Rh/RhDireccionesUpdate.cs
+++ Convertidor/Dtos/Rh/RhDireccionesUpdate.cs
@@ -10,12 +10,12 @@ public class RhDireccionesUpdate
     public int MunicipioId { get; set; }
     public int CiudadId { get; set; }
     public int ParroquiaId { get; set; }
-    public int SectorId { get; set; }
-    public int UrbanizacionId { get; set; }
-    public int ManzanaId { get; set; }
-    public int ParcelaId { get; set; }
-    public int VialidadId { get; set; }
-    public string Vialidad { get; set; } = string.Empty;
+   // public int SectorId { get; set; }
+    //public int UrbanizacionId { get; set; }
+    //public int ManzanaId { get; set; }
+    //public int ParcelaId { get; set; }
+    //public int VialidadId { get; set; }
+    //public string Vialidad { get; set; } = string.Empty;
     public int TipoViviendaId { get; set; }
     public int ViviendaId { get; set; }
     public string Vivienda { get; set; } = string.Empty;
@@ -25,6 +25,6 @@ public class RhDireccionesUpdate
     public string ComplementoDir { get; set; } = string.Empty;
     public int TenenciaId { get; set; }
     public int CodigoPostal { get; set; }
-    public int Principal { get; set; }
+    public bool Principal { get; set; }
   
 }
