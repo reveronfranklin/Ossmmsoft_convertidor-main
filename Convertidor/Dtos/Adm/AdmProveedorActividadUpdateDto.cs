37b951a0391f6f5538351cd2bfb1513d86733265
reveronfranklin <reveronfranklin@gmail.com>
2025-12-09 13:19:50 -0400

    Add crud Actividad Proveedor

--- Convertidor/Dtos/Adm/AdmProveedorActividadUpdateDto.cs
+++ Convertidor/Dtos/Adm/AdmProveedorActividadUpdateDto.cs
@@ -6,9 +6,7 @@ public class AdmProveedorActividadUpdateDto
     public int CodigoProveedor  { get; set; } 
     public int ActividadId { get; set; }
     public DateTime FechaIni { get; set; } 
-    public string FechaIniString { get; set; }  = string.Empty;
-    public FechaDto FechaIniObj { get; set; } 
+
     public DateTime FechaFin { get; set; }
-    public string FechaFinString { get; set; } = string.Empty;
-    public FechaDto FechaFinObj { get; set; } 
+
 }
