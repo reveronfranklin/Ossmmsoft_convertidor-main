7dd77b6ea9a9108d233b1857ac0958f504063b8a
reveronfranklin <reveronfranklin@gmail.com>
2026-05-26 19:44:43 -0400

    corregir dto

--- Convertidor/Controllers/RhTipoNominaController.cs
+++ Convertidor/Controllers/RhTipoNominaController.cs
@@ -60,7 +60,7 @@ namespace Convertidor.Controllers
         // Cuerpo de la solicitud en formato JSON
         var requestBody = new
         {
-            CodigoTipoNomina = dto.CodigoTipoNomina,
+            codigoTipoNomina = dto.CodigoTipoNomina,
         };
 
         // Convertir el cuerpo a JSON
